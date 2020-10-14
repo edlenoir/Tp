ad00690a03263603a905e0d9b7ab79b9fd9300bf
Florian Lombard <flolombard@hotmail.fr>
2020-10-14 15:14:16 +0200

    "TP3 finit attention (colonne , ligne)

--- TP3_M3103/src/main.java
+++ TP3_M3103/src/main.java
@@ -1,7 +1,10 @@
+import java.io.ObjectInputStream.GetField;
 
 public class main {
 	public static void main(String[] args)  {
-	Labyrinthe l = new Labyrinthe() ;
+	Labyrinthe l = new Labyrinthe();
 	l.afficherlab();
+	l.existeChemin(6, 8);
+	System.out.println(l.getDirection());
 }
 }
